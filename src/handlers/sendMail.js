6d59177503993445fd26dce42c357c44de0ab393
Anže Stražar <anze.strazar@tretton37.com>
2021-03-03 08:56:11 +0100

    Send test email

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,6 +1,32 @@
+import AWS from "aws-sdk";
+
+const ses = new AWS.SES({ region: "eu-west-1" });
+
 async function sendMail(event, context) {
-  console.log(event);
-  return event;
+  const params = {
+    Source: "strazar@gmail.com",
+    Destination: {
+      ToAddresses: ["strazar@gmail.com"],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: "Hello from Mars!",
+        },
+      },
+      Subject: {
+        Data: "Test Mail",
+      },
+    },
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export const handler = sendMail;
